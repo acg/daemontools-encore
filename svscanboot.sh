d6cd619f588b0142039397627d9ebdb34bb3db3a
Alan Grow <alangrow@gmail.com>
2016-08-19 14:16:27 -0600

    auto-export everything in /etc/environment.

--- svscanboot.sh
+++ svscanboot.sh
@@ -4,7 +4,9 @@ PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin
 test -z "$SVSCANBOOT" && exec env - PATH="$PATH" SVSCANBOOT="$0" "$0" "$@"
 unset SVSCANBOOT
 
+set -a
 test -f /etc/environment && . /etc/environment
+set +a
 
 exec </dev/null
 exec >/dev/null
